7a9e0b2d7b73df8039e705e2f74f6fb806501b85
kimpeek <harrelchris@gmail.com>
2018-06-14 15:18:42 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 function getData () {
     return {
         key1: "value1",
-        key2: "value2"
+        key2: "value2",
+        key3: "value3",
+        key4: "value4",
+        key5: "value5",
+        key6: "value6"
     }
 }
 
 
 function inputData (data) {
-    // make new row
+    // for each row in data
+    // insert a new row
+}
+
+
+function insertRow(key, value){
     const table = document.getElementById("tbody");
     const row = table.insertRow(0);
-    // make two columns
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
-    // insert keys and values into their columns
-    cell1.innerHTML = "New cell";
-    cell2.innerHTML = "New cell2";
+    cell1.innerHTML = key;
+    cell2.innerHTML = value;
 }
 
 
